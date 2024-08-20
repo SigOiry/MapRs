517a114412d8e02d1a068ce2dd39677c13292c48
Simon <oirysimon@gmail.com>
2024-08-20 15:21:55 +0200

    optimized clean_spectra

--- R/Clean_Spectral_library.R
+++ R/Clean_Spectral_library.R
@@ -45,7 +45,7 @@ clean_spectra <- function(input_data) {
                                 choices = unique(input_data$ID), server = TRUE)
     
     output$spectra_plot <- plotly::renderPlotly({
-      plotly::plot_ly(spectra_data(), x = ~Wavelength, y = ~Value,
+      plotly::plot_ly(spectra_data(), x = ~Wavelength, y = ~Value, color = ID, colors = "YlOrRd",
                       type = 'scatter', mode = 'lines', hoverinfo = 'text',
                       text = ~paste("Spectrum:", ID)) %>%
         plotly::layout(showlegend = F)
