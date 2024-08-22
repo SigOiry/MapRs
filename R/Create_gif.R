c96b858862928f2b4442220ee6c11dd1c34b2672
Simon <oirysimon@gmail.com>
2024-08-22 13:30:58 +0200

    create gif

--- R/Create_gif.R
+++ R/Create_gif.R
@@ -1,6 +1,11 @@
 # Function to create GIF from a list of images
-create_gif <- function(image_list, output_file = NA, fps = 10, loop = 0) {
+create_gif <- function(image_list = NA, output_file = NA, fps = 10, loop = 0) {
   # Read the images into a magick image object
+  
+  if(is.na(image_list)){
+    path <- rstudioapi::selectDirectory(caption = "Path to the folder where the images are")
+    image_list <- list.files(path, full.names = T)
+  }
   images <- magick::image_read(image_list)
   
   # Create the GIF
